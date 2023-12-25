b75209d913abba01823702305f657e2e14d71979
xzdil <adil3201@outlook.com>
2023-12-25 08:32:32 +0600

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from model import generate
 app = FastAPI()
 
 
 @app.get("/")
+def load():
+    return FileResponse("index.html")
+
+@app.post("/")
 def read_root(text: str):
     return generate(text)
 
